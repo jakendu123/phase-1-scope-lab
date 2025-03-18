c4d03294700324f2ec8255fa089576e5b4a33062
Fredrick Okoth <fredrick.okoth1@student.moringaschool.com>
2025-03-18 22:31:17 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new least favorite";
+}
+
+let var1 = 1;
+function firstFunc(var2 = 2) {
+  let var3 = 3;
+  return var1 + var2;
+}
+console.log(firstFunc(2, 9));
